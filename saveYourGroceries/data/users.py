0bfb944f5222af19dc45a2e5d6e7fae9a4db365d
Jack Wang <ziqianjwang@utexas.edu>
2021-01-13 21:46:42 -0600

    login,create user page. add_user method

--- saveYourGroceries/data/users.py
+++ saveYourGroceries/data/users.py
@@ -1,6 +1,7 @@
 from os import getenv 
 from os.path import join, dirname
 from dotenv import load_dotenv 
+from bson.objectid import ObjectId
 
 dotenv_path = join(dirname(__file__), '.env')
 load_dotenv(dotenv_path)
@@ -10,6 +11,13 @@ from pymongo import MongoClient
 
 client = MongoClient(f"mongodb+srv://zjackwang:{mongo_key}@cluster0.5ocd6.mongodb.net/test?authSource=admin&replicaSet=atlas-q2c9r8-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true")
 users_db = client['users']
+
+def find_user(_username):
+    query = {
+        'username': _username
+    }
+    user = users_db.users.find_one(query)
+    return user
 
 # if __name__ == "__main__": 
 #     users = users_db.users
--- saveYourGroceries/routes.py
+++ saveYourGroceries/routes.py
@@ -1,12 +1,42 @@
-from flask import render_template, request 
+from flask import render_template, request, jsonify, redirect
+from bson.json_util import dumps
+from bson.objectid import ObjectId 
+from werkzeug.security import generate_password_hash, check_password_hash
 
 from saveYourGroceries.app import app
+from saveYourGroceries.data.users import users_db, find_user
 
 @app.route('/') 
 def index():
     return render_template("index.html")
 
-
 @app.route('/login')
 def login():
     return render_template("login.html")
+
+@app.route('/create')
+def create(): 
+    return render_template("create.html")
+
+@app.route('/adduser', methods=['POST'])
+def add_user():
+    if request.method == 'POST':
+        _username = request.form['inputUsername']
+        if find_user(_username):
+            # how to send message? 
+            return redirect('/create')
+
+        _password = request.form['inputPassword']
+        _name = request.form['inputName']
+        _email = request.form['inputEmail']
+        _hashed_pwd = generate_password_hash(_password)
+        query = {
+            'name': _name,
+            'email': _email,
+            'username': _username,
+            'password': _hashed_pwd
+        }
+        users_db.users.insert_one(query)
+        return redirect('/login')
+    else: 
+        return not_found()
