685792746108a663e0cd7cebb8cda8db6215d968
Jack Wang <ziqianjwang@utexas.edu>
2021-02-20 18:52:18 -0600

    Basic matching with current product database

--- saveYourGroceries/data/grocery.py
+++ saveYourGroceries/data/grocery.py
@@ -1,13 +1,15 @@
-from saveYourGroceries.data.db import groceries_db 
 import datetime 
+from saveYourGroceries.data.db import groceries_db, dates_db, products_db
 
-def add_groceries(user, item, date):
+
+def add_groceries(user, item, date, exp_date):
     if date == None:
         date = datetime.date.today().strftime('%Y/%m/%d')
     query = { 
         'user': user,
         'item': item,
-        'date_purchased': date
+        'date_purchased': date,
+        'date_expiration': exp_date
     }
 
     groceries_db.insert_one(query) 
@@ -26,3 +28,13 @@ def remove_grocery(user, item):
     }
 
     groceries_db.delete_one(query)
+
+def get_product_fridge_life(item):
+    query = {
+        'product': item
+    }
+
+    return dates_db.find(query)[0]['days_in_fridge']
+
+def get_product_list():
+    return products_db.find({'name':'products'})[0]['products']
--- saveYourGroceries/routes.py
+++ saveYourGroceries/routes.py
@@ -2,7 +2,10 @@ from flask import render_template, flash, request, jsonify, redirect, url_for
 from flask_login import current_user, login_user, logout_user, login_required
 from werkzeug.urls import url_parse
 from werkzeug.security import generate_password_hash, check_password_hash
+
 import json 
+import re
+from datetime import date, timedelta
 
 from saveYourGroceries.app import app
 from saveYourGroceries.data.user import User 
@@ -12,6 +15,7 @@ from saveYourGroceries.rparser import analyze_receipt
 import saveYourGroceries.data.login
 import saveYourGroceries.config 
 from saveYourGroceries.data import grocery
+
 
 
 @app.route('/', methods=['GET', 'POST']) 
@@ -71,32 +75,60 @@ def register():
 def scan():
     return render_template("scan.html")
 
+
+"""
+Finds only exact delimited words 
+@param item: str, grocery item name
+@param purchase_date: date object, when item was purchase
+@param product_list: list, all products in expiration database
+@return date_item: datetime, day of recommended grocery usage 
+"""
+def process_grocery_exp(item, purchase_date, product_list):
+    DEFAULT_DAYS = timedelta(days=7)
+    for product in product_list: 
+        c = re.compile(f"(?:^|\W){product}(?:$|\W)")
+        item_lower = item.lower()
+        if len(c.findall(item_lower)) > 0:
+            days = grocery.get_product_fridge_life(product)
+            # use timedelta to change date
+            delta_t = timedelta(days=days)
+            return purchase_date + delta_t
+
+    return purchase_date + DEFAULT_DAYS
+
+
 @app.route('/upload', methods=['POST'])
 def upload():
     if 'image' not in request.files:
         flash("No file part")
         return redirect(url_for('scan'))
-    file = request.files['image']
-    if file.filename == '':
+    
+    receipt = request.files['image']
+    if receipt.filename == '':
         flash("No image selected for uploading")
         return redirect(url_for('scan'))
-    
-    
-    receipt_json = analyze_receipt(file)
 
+    receipt_json = analyze_receipt(receipt)
     r_info = receipt_json['analyzeResult']['documentResults'][0]['fields']
     store_name = r_info['MerchantName']['text'] if r_info['MerchantName']['confidence'] > .5 else None 
     store_products = [(obj['valueObject']['Name']['text'], obj['valueObject']['TotalPrice']['text']) for obj in r_info['Items']['valueArray']]
 
-    date = None 
-    
     if len(receipt_json['analyzeResult']['documentResults'][0]['fields']) > 4:
-        date = receipt_json['analyzeResult']['documentResults'][0]['fields']['TransactionDate']['valueDate']
+        r_date = receipt_json['analyzeResult']['documentResults'][0]['fields']['TransactionDate']['valueDate'],split('/')
+        p_date = date(r_date[0], r_date[1], r_date[2])
+    else:
+        p_date = date.today()
 
+    # omit prices for now 
     items = [item for item, price in store_products]
-
+    # get specific grocery list 
+    product_list = grocery.get_product_list()
+    # try to match each item to one on list
     for item in items: 
-        grocery.add_groceries(current_user.username, item, date) 
+        exp_date = process_grocery_exp(item, p_date, product_list)
+        # store ordinal 
+        print(f"Storing {item} with {str(exp_date)} exp date")
+        grocery.add_groceries(current_user.username, item, p_date.toordinal(), exp_date.toordinal())
 
     flash(f"Success! You have added {len(items)} groceries to your account")
     return redirect(url_for("scan"))
